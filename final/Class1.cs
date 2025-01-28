5ab6b4ebb28af6f66a17abfb0bb14a09ec0aed74
praimash <umutohri20@gmail.com>
2025-01-29 02:12:37 +0300

    Proje dosyası ekle.

---
+++ final/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using final;
+
+
+namespace final
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Gamemenu gamemenu = new Gamemenu();
+            begin begin = new begin();
+            GameMap gameMap = new GameMap();
+            begin.start();
+            gamemenu.sentence1();
+
+            Console.WriteLine("\nGame is starting... Explore the map!");
+            Console.WriteLine("Use W (up), A (left), S (down), D (right) to move.");
+            Console.WriteLine("Type 'exit' to quit the game.");
+
+            while (true)
+            {
+                gameMap.healthbar();
+                gameMap.DisplayMap();
+                Console.Write("\nYour move (W/A/S/D): ");
+                string input = Console.ReadLine()?.ToLower();
+                if (input == "exit")
+                    break;
+                gameMap.MovePlayer(input);
+                Console.WriteLine("\nPress any key to continue...");
+                Console.ReadKey();
+            }
+            Console.WriteLine("Game Over. Thanks for playing!");
+        }
+    }
+}
